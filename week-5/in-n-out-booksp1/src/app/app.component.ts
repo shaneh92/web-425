1b3d6dccfd02f730623ab9a091974e86bec92549
Shane Hingtgen <shanehingtgen@gmail.com>
2023-06-20 21:58:17 -0500

    exercise 5.2 navigation and layouts

---
+++ week-5/in-n-out-booksp1/src/app/app.component.ts
@@ -0,0 +1,31 @@
+/*
+    ========================================================================================================================
+    ; Title: app.component.ts
+    ; Author: Shane Hingtgen
+    ; Bellevue University
+    ; Date: 6/20/23
+    ; Description: This purpose of this assignment is create new navigation and layout based on flex layout and angular materials
+    ; Work Cited:
+    Coding Guidelines
+    Instructions - Exercise 5.1 - Navigation and Layout
+    Angular Tutorial video https://www.youtube.com/watch?v=J2RHvuYFna4
+    Angular Tutorial video https://www.youtube.com/watch?v=Wif3MsDN7Pg
+    Course GitHub
+    ========================================================================================================================
+ */
+
+import { Component } from '@angular/core';
+import { AppModule } from './app.module';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  assignment: string;
+
+  constructor() {
+    this.assignment = 'Exercise 5.3 - Navigation and Layout';
+  }
+}
---
+++ week-5/in-n-out-booksp1/src/app/book-list/book-list.component.ts
@@ -0,0 +1,28 @@
+/*
+    ========================================================================================================================
+    ; Title: book-list.component.ts
+    ; Author: Shane Hingtgen
+    ; Bellevue University
+    ; Date: 6/20/23
+    ; Description: This purpose of this assignment is create new navigation and layout based on flex layout and angular materials
+    ; Work Cited:
+    Coding Guidelines
+    Instructions - Exercise 5.1 - Navigation and Layout
+    Angular Tutorial video https://www.youtube.com/watch?v=J2RHvuYFna4
+    Angular Tutorial video https://www.youtube.com/watch?v=Wif3MsDN7Pg
+    Course GitHub
+    ========================================================================================================================
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss'],
+})
+export class BookListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
